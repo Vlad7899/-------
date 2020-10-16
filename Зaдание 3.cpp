6240f6bb3d7dee596a601321d4085f85257f5f4d
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-10-16 18:38:50 +0300

    Add files via upload

---
+++ Зaдание 3.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N = 11;  
+    int W = 12;  
+    int S = 13;  
+    int E = 14;
+    int a;
+    int n1=0;
+    int n2=1;
+    int n3=-1;
+    int b;
+    cin >> a;
+    cin >> b;
+    if (a == N && b == n1)
+        cout << "robot looks and goes North";
+    else if (a == N && b == n2)
+        cout << "robot looks and turns West";
+    else if (a == N && b == n3)
+        cout << "robot looks and turns East";
+    else if (a == W && b == n1)
+        cout << "robot looks and goes West";
+    else if (a == W && b == n2)
+        cout << "robot looks and turns South";
+    else if (a == W && b == n3)
+        cout << "robot looks and turns North";
+    else if (a == S && b == n1)
+        cout << "robot looks and goes South";
+    else if (a == S && b == n2)
+        cout << "robot looks and turns East";
+    else if (a == S && b == n3)
+        cout << "robot looks and turns West";
+    else if (a == E && b == n1)
+        cout << "robot looks and goes East";
+    else if (a == E && b == n2)
+        cout << "robot looks and turns North";
+    else if (a == E && b == n3)
+        cout << "robot looks and turns South";
+    else
+        cout << "error";
+    return 0;
+}
