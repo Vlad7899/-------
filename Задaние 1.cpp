4fcb67b716c31732786de79d571f104bcdb465bd
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-10-23 20:12:43 +0300

    Add files via upload

---
+++ Задaние 1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Distday = 10;
+    int Day = 0;
+    for (int a = 1; a <= 1000; a++)
+        Day += Distday;
+    Distday += Distday * 0, 1;
+    if (Day == 2)
+        cout << Distday << "for 2 day";
+    else if (Day == 3)
+        cout << Distday << "for 3 day";
+    else if (Day == 10)
+         cout << Distday << "for 10 day";
+        return 0;
+    
+}
+                         
+
+
+
+
+
+
+
+
+
+
+
+
+
