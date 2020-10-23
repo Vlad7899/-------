f03849731fa524709bde766186d926d03b2156dd
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-10-23 20:13:29 +0300

    Add files via upload

---
+++ Задание 1 в.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int sumRun()
+{
+    int distance;
+    int sumDays = distance;
+    int days;
+    for (int i = days; i > 1; --i)
+    {
+        distance += distance * 0.1;
+        sumDays += distance;
+    }
+    return 0;
+}
