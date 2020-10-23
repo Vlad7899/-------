711ec85786ab9da25b04d6e2a32afda26e3bdb97
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-10-23 20:13:02 +0300

    Add files via upload

---
+++ Задание 1 б.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double distance = 10, total = 0;
+    int day = 1;
+    cout << "Day\tDistance" << endl;
+    do
+    {
+        cout << day << '\t' << distance << endl;
+        total += distance;
+        distance += 0.1 * distance;
+        ++day;
+    } while (day <= 7);
+    cout << "Total: " << total;
+    return 0;
+}
