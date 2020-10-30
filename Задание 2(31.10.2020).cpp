bec42b0f145fe0288acb8c7dae2788f70752dafe
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-10-31 00:02:42 +0300

    Add files via upload

---
+++ Задание 2(31.10.2020).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a, max;
+    cin>>a;
+    max = a % 10;
+    a = a / 10;
+    while (a > 0) {
+        if (a % 10 > max)
+            max = a % 10;
+        a = a / 10;
+    }
+    cout<<max;
+    if (a % 2 == 0) {
+        cout <<"prime number";
+    }
+}
