76d748011f265f811b537a213e742fa0deedb589
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-10-30 23:49:27 +0300

    Add files via upload

---
+++ Задание 2(31.10.2020).cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a, max;
+    cin>>a;
+    max = a % 10;
+    a = a / 10;
+    while (a > 0) {
+        if (a % 10 > max)
+            max = a % 10;
+        a = a / 10;
+    }
+    cout<<max;
+}
