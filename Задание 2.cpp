a328ad4ec4c63bfb8098e7c7d65fee4c392f5ecb
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-11-20 20:39:07 +0300

    Add files via upload

---
+++ Задание 2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int b=10;
+	for (int i = 1; i < 10; i++) {
+		int a[b];
+		int num=0;
+		if (a[b] % 2 = 0)
+			cout << '1';
+		else
+			num = a[b] % i;
+		cout << num;
+	}
+
+}
