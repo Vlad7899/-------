d75a19895b48574e61aec4fec72ed03ba0174903
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-11-21 13:33:46 +0300

    Add files via upload

---
+++ Задание 1(21.11.20).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int i=9;
+	int arr[i] = {2,3,4,5,6,7,8,9,10};
+	for (int a = 0; a <= 9; a++) {
+		if (arr[i] % 2 == 0) {
+			arr[i] = 0;
+		}
+		else(arr[i] % 2 != 0) {
+			arr[i] = 1;
+		}
+	}
+	for (int a = 0; a <= 9; a++) {
+		cout << arr[i]<<' ';
+	}
+}
