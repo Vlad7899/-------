5bc8d9d076dc5fab6fea3a52d8475a3d768ca697
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-11-28 13:21:53 +0300

    Add files via upload

---
+++ Задaние 2(28.11.20).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[11] = {};
+	int x;
+	cin >> x;
+	for (int i = 0; i <= 10; i++) {
+		cin >> arr[i];
+		if (x == arr[i]) {
+			cout << "Num exist";
+			break;
+		}
+		else
+		{
+			cout << "Num dont exist";
+			break;
+		}
+	}
+}
