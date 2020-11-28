fc6a6f8b08f2008df6a79398c5f365442c39913b
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-11-28 13:15:42 +0300

    Add files via upload

---
+++ Задание 1(28.11.20).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int arr[9] = {};
+	for (int i = 0; i <= 9; i++) {
+		arr[i] = i * i;
+	}
+	for (int i = 0; i <= 9; i++) {
+		cout << arr[i] << ' ';
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
