c00da9b6e18278f5445edfeb404cc0f17e563681
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-12-05 12:59:50 +0300

    Add files via upload

---
+++ Задание 1(05.12.20).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int arr[10];
+	int buf;
+	srand(time(0));
+	for (int i = 0; i < 10; i++) {
+		arr[i] = rand() % 20;
+	}
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++) {
+			{
+				if (arr[i] <= arr[i + 1]) {
+					arr[i] = buf;
+					arr[i + 1] = arr[i];
+					arr[i + 1] = buf;
+				}
+			}
+		}
+	for (int i = 0; i < 10; i++) {
+		cout << arr[i] << ' ';
+	}
+	return 0;
+}
