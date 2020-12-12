ccbf5362e5fee17a1fcd64b124f85b862ad56fb8
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-12-12 12:36:45 +0300

    Add files via upload

---
+++ Задание 2(12.12.20).cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[] = {};
+	int max = 0;
+	for (int i = 0; i < 10; i++) {
+		arr[i] = 1 + rand() % 20;
+		if (arr[i] % 2 != 0 & arr[i] > max & arr[i] % 3 = 0) {
+			max = arr[i];
+		}
+	}
+	cout << max;
+}
