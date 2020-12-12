f9ab7eadfaf20e322890d144347c5608731512ca
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-12-12 12:36:21 +0300

    Add files via upload

---
+++ Задание 1(12.12.20).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int arr[11] = {};
+	int max,min,buf;
+	srand(time(0));
+	for (int i = 0; i < 10; i++) {
+		arr[i] = 1 + rand() % 20;
+		if (arr[i] % 2 = 0 & max < arr[i])
+			max = arr[i];
+		else if(arr[i] % 2 = 0 & min > arr[i])
+			min = arr[i];
+	}
+	for (int i = 0; i < 1; i++) {
+		buf = max;
+		max = min;
+		min = buf;
+	}
+}
+
