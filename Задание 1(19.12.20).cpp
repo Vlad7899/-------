45aaa4ba747643b4a79445e44ea06c0bd4a29650
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2020-12-19 12:47:16 +0300

    Add files via upload

---
+++ Задание 1(19.12.20).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand(time(0));
+	int arr[10] = {};
+	int max,n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 50;
+		cout << arr[i], ' ';
+	}
+		for (int i = 1; i < n - 1; i++)
+		{
+			if (arr[i] > arr[i + 1] && arr[i] > arr[i - 1])
+			{
+				max = arr[i];
+				cout << "Local max: " << max << endl;
+			}
+		}
+}
