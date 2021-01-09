c26afb063b895bae0eb8ffcd9c87023d5e1e931b
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2021-01-09 13:04:32 +0300

    Add files via upload

---
+++ Задание 2(09.01.21).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	int numcount;
+	const int num = 5;
+	cin >> k;
+	for (int i = 0; 1 < k; i++) {
+	    cin >> n;
+		if (num > n) {
+			cout << "Number " << n << " greater than";
+		}
+		else if (num < n) {
+			cout << "Number " << n << " is less than";
+		}
+		else if (num == n) {
+			cout << "You win\n";
+			numcount = 1;
+			break;
+		}
+	}
+	if (numcount == 1)
+		cout << "You guessed it";
+}
