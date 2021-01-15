d127abc140a896da90083f1c1ae8844d9219dac4
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2021-01-15 19:34:17 +0300

    Add files via upload

---
+++ Дз/Задание 8(16.01.21) одномерный массив.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int arr[10];
+	srand(time(0));
+	for (int i = 0; i < 10; i++) {
+		arr[i] = 1 + rand() % 10;
+		if (i % 2 == 0) {
+			arr[i] = 0;
+		}		
+	}
+	for (int i = 0; i < 10; i++) {
+		cout << arr[i] << ' ';
+	}
+}
