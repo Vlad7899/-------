a07711941d91ddc4ec400ca85bf72ebf298d29cb
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2021-01-15 19:27:09 +0300

    Add files via upload

---
+++ Дз/Задание 3(16.01.21) двумерный массив.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int numOne = 5;
+	int numTwo = 4;
+	int* ptArr;
+	ptArr = new int[5 * 4];
+	srand(time(0));
+	for (int i = 0; i < numOne; i++) {
+		for (int j = 0; j < numTwo; j++) {
+			ptArr[i * j] = 1 + rand() % 20;
+			cout << ptArr[i * j]<<' ';
+		}
+	}
+	cout << endl;
+	for (int i = 0; i < numOne; i++) {
+		for (int j = 0; j < numTwo+1; j++) {
+			cout << ptArr[i * j] << ' ';
+		}
+	}
+}
