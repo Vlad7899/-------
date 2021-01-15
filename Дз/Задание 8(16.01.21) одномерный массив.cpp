dae3ecea37d7f9c3d617e27ef44bc49305875d9a
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2021-01-15 20:03:12 +0300

    Update Задание 8(16.01.21) одномерный массив.cpp

--- Дз/Задание 8(16.01.21) одномерный массив.cpp
+++ Дз/Задание 8(16.01.21) одномерный массив.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	int arr[10];
+	int *ptArr = new int[10];
 	srand(time(0));
 	for (int i = 0; i < 10; i++) {
 		arr[i] = 1 + rand() % 10;
@@ -15,4 +15,5 @@ int main()
 	for (int i = 0; i < 10; i++) {
 		cout << arr[i] << ' ';
 	}
+	delete[] ptArr; 
 }
