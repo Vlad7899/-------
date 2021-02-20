e02a86093963ab65ced15a6835d02017b573e9f9
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2021-02-20 13:43:36 +0300

    Add files via upload

--- Дз(30.01.21)/Задание 1(20.02.21).cpp
+++ Дз(30.01.21)/Задание 1(20.02.21).cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <ctime>
+#include <cstring>
 
 using namespace std;
 
@@ -7,6 +8,8 @@ struct List {
     int value;
     List* p;
 };
+
+int n;
 
 void getm(int k, List* q) {
     List* t, * t1;
@@ -17,12 +20,47 @@ void getm(int k, List* q) {
     }
     cout << "value = " << t->value << endl;
 }
+List* searchElem(int value, List* q) {
+    List* t, * t1, * searchpointer;
+    t = q;
+    for (int i = 0; i < n; i++) {
+        if (value == t->value) {
+            searchpointer = t;
+            return searchpointer;
+    }
+        t1 = t->p;
+        t = t1;
+    }
+    return "Nothing.";
+}
+void removeElem(int k, List* q) {
+    List* t, * t1, * beforeElem;
+    t = q;
+    for (int i = 0; i < k; i++) {
+        if (i == k - 1) {
+            beforeElem = t;
+    }
+        t1 = t->p;
+        t = t1;
+    }
+    beforeElem->p = t->p;
+    delete t;
+    n = n - 1;
+}
+void printList(List* q) {
+    List* t, * t1;
+    t = q;
+    for (int i = 0; i < k; i++) {
+        t1 = t->p;
+        cout << i + 1 << ":value=" << t->value << "\n";
+        t = t1;
+    }
+
+}
 
 int main() {
     srand(time(0));
     int n;
-    int num = 5;
-    int num1 = 10;
     List* q0, * q1, * q2;
 
     q0 = new List;
@@ -39,18 +77,7 @@ int main() {
     }
     q1->value = rand() % 100;
     q1->p = q0;
-    for (int i = 1; i < n; i++) {
-        q1->value = rand() % 100;
-        cout << q1->value;
-        if (q1->value == num1)
-        {
-            continue;
-        }
-    }
-    if (q1->value == num)   
-        cout << "Num found:"<<q1->value;
-    else
-        cout << "Not found";
+
     do {
         int k;
         cout << "input index or 0 to exit" << endl;
@@ -64,9 +91,9 @@ int main() {
             return 0;
         }
         getm(k, q0);
+        printList(q0);
+        cout << searchElem(5, q0) << endl;
+        removeElem(5, q0);
+        printList(q0);
     } while (true);
-    for (int i = 1; i < n; i++) {
-        q1->value = rand() % 100;
-        cout << q1->value;
-    }
 }
