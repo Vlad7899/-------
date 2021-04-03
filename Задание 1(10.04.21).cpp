6f01707a2817b4e5b886c4aff9205b9c401e365d
Vlad7899 <72569020+Vlad7899@users.noreply.github.com>
2021-04-03 16:26:27 +0300

    Add files via upload

---
+++ Задание 1(10.04.21).cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand(time(0));
+	bool flag;
+	char n;
+	char str[20];
+	cout << "Symbol:";
+	cin >> n;
+	for (int i = 0; i < 20; i++) {
+    str[i] = 'A' + rand() % ('z' - 'A');
+	}
+	for (int i = 0; i < 20; i++) {
+		cout << str[i];
+		if (n == str[i]) {
+			cout << "Index of symbol:" << i;
+			flag = true;
+		}
+		else if (flag != true)
+			cout << "Not found";
+	}
+	return 0;
+}
